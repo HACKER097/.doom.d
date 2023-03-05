428333a756a9b69fd079666b35babb6a8587bb19
HACKER097 <mr.hackey009977@gmail.com>
2023-03-05 16:10:28 +0530

    first commit

---
+++ config.el
@@ -0,0 +1,154 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Shantanu Bhargava"
+      user-mail-address "shantanubhargava2005@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;;(set-fontset-font 'doom-dashboard-banner (font-spec :family "Hack":size 20))
+
+
+(custom-theme-set-faces! 'doom-tokyo-night
+  '(doom-dashboard-banner  :height 250)
+  '(doom-dashboard-footer :inherit font-lock-constant-face)
+  '(doom-dashboard-footer-icon :inherit all-the-icons-red)
+  '(doom-dashboard-loaded :inherit font-lock-warning-face)
+  '(doom-dashboard-menu-desc :inherit font-lock-string-face)
+  '(doom-dashboard-menu-title :inherit font-lock-function-name-face))
+
+(custom-set-variables
+ '(mini-frame-show-parameters
+   '((top . 10)
+     (width . 0.7)
+     (left . 0.5))))
+
+(defun doom-dashboard-draw-ascii-banner-fn ()
+  (let* ((banner
+                '("______ _____ ____ ___ ___"
+                "`  _  V  _  V  _ \\|  V  ´"
+                "| | | | | | | | | |     |"
+                "| | | | | | | | | | . . |"
+                "| |/ / \\ \\| | |/ /\\ |V| |"
+                "|   /   \\__/ \\__/  \\| | |"
+                "|  /                ' | |"
+                "| /      ई मे क् स      \\ |"
+                "‾‾                     ‾‾"))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                (/ +doom-dashboard--width 2.5) ;; Have to re-align the banner manually in respect to scaling it
+                (concat
+                 line (make-string (max 0 (- longest-line (length line)))
+                                   32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))
+
+;; To remove top padding let's override that fn too
+(defun +doom-dashboard-resize-h (&rest _)
+  "Recenter the dashboard, and reset its margins and fringes."
+  (let (buffer-list-update-hook
+        window-configuration-change-hook
+        window-size-change-functions)
+    (when-let (windows (get-buffer-window-list (doom-fallback-buffer) nil t))
+      (dolist (win windows)
+        (set-window-start win 0)
+        (set-window-fringes win 0 0)
+        (set-window-margins win (max 0 (/ (- (window-total-width win) +doom-dashboard--width) 2))))
+      (with-current-buffer (doom-fallback-buffer)
+        (save-excursion
+          (with-silent-modifications
+            (goto-char (point-min))
+            (delete-region (line-beginning-position)
+                           (save-excursion (skip-chars-forward "\n")
+                                           (point)))
+            ))))))
+
+(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-footer)
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(mini-frame-mode)
+
+(setq-default cursor-type 'bar)
+
+(setq doom-theme 'doom-catppuccin)
+(setq doom-catppuccin-dark-variant "mocha") ;; "latte", "frappe" or "macchiato"
+
+(setq org-babel-python-command "python3.10")
---
+++ custom.el
@@ -0,0 +1,62 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#191919" "#FF5E5E" "#468800" "#E9FDAC" "#8CDAFF" "#C586C0" "#85DDFF" "#d4d4d4"])
+ '(custom-enabled-themes '(doom-tokyo-night))
+ '(custom-safe-themes
+   '("60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "f458b92de1f6cf0bdda6bce23433877e94816c3364b821eb4ea9852112f5d7dc" "016f665c0dd5f76f8404124482a0b13a573d17e92ff4eb36a66b409f4d1da410" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "683b3fe1689da78a4e64d3ddfce90f2c19eb2d8ab1bab1738a63d8263119c3f4" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default))
+ '(exwm-floating-border-color "#121212")
+ '(fci-rule-color "#515151")
+ '(highlight-tail-colors ((("#1d2416" "#1d2416") . 0) (("#232c30" "#202c30") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#171F24" "#FFFFFF"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#171F24" "#468800"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#171F24" "#777778"))
+ '(mini-frame-mode t)
+ '(mini-frame-show-parameters '((top . 10) (width . 0.7) (left . 0.5)))
+ '(objed-cursor-color "#FF5E5E")
+ '(package-selected-packages
+   '(ox-twbs catppuccin-theme mini-frame org-roam-ui org-roam exwm unicode-fonts ox-pandoc org-beautify-theme))
+ '(pdf-view-midnight-colors (cons "#d4d4d4" "#191919"))
+ '(rustic-ansi-faces
+   ["#191919" "#FF5E5E" "#468800" "#E9FDAC" "#8CDAFF" "#C586C0" "#85DDFF" "#d4d4d4"])
+ '(vc-annotate-background "#191919")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#468800")
+    (cons 40 "#7caf39")
+    (cons 60 "#b2d672")
+    (cons 80 "#E9FDAC")
+    (cons 100 "#efd98e")
+    (cons 120 "#f5b671")
+    (cons 140 "#FC9354")
+    (cons 160 "#e98e78")
+    (cons 180 "#d78a9c")
+    (cons 200 "#C586C0")
+    (cons 220 "#d8789f")
+    (cons 240 "#eb6b7e")
+    (cons 260 "#FF5E5E")
+    (cons 280 "#dd6464")
+    (cons 300 "#bb6a6b")
+    (cons 320 "#997071")
+    (cons 340 "#515151")
+    (cons 360 "#515151")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+ '(org-level-10 ((t (:inherit outline-10 :height 1.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.05))))
+ '(org-level-6 ((t (:inherit outline-6 :height 1.0))))
+ '(org-level-7 ((t (:inherit outline-7 :height 1.0))))
+ '(org-level-8 ((t (:inherit outline-8 :height 1.0))))
+ '(org-level-9 ((t (:inherit outline-9 :height 1.0)))))
+(put 'customize-themes 'disabled nil)
